6ac14952967d21155cd1f52154a002dc5719c938
shablargablargn <shablargablargn@Jonahs-Laptop.localdomain>
2023-09-04 14:32:03 -0500

    have the initial done

--- index.js
+++ index.js
@@ -4,40 +4,52 @@ const state = {
     pups: [],
 };
 
-// async function render() {
-//     await getPups();
-//     // CanvasRenderingContext2D();
-// }
-// render();
+
+const pupList = document.querySelector("#pupHeads");
+//console.log(typeof state.pups.data);
 
 async function getPups() {
     const response = await fetch(API_URL);
     const json = await response.json();
-    state.pups.players = json.data;
+    console.log(json);
+    state.pups = json.data.players;
+    console.log(state.pups);
 }
 
-
-console.log(getPups());
-
-
 function renderPups() {
-    console.log(state.pups)
-    console.log(typeof state.pups.players);
-    const pupCards = state.pups.players.map((pup) => {
-        const pupLink = document.createElement("link");
+    //console.log(state.pups)
+    //console.log(typeof state.pups);
+    const pupCards = state.pups.forEach((pup) => {
+        const pupLink = document.createElement("div");
         pupLink.innerHTML = `
             <h2>
             <a href="#${pup.id}">
             ${pup.name}
             </a>
             </h2>
-            `
-    }).join('');
-    return pupCards;
+            <h3>
+            ${pup.breed}
+            </h3>
+            `;
+            pupList.appendChild(pupLink);
 
+        return pupLink;
+        
+        }).join('');
+    pupList.replaceChildren(pupCards);
     
+    pupList.addEventListener("click", () => {
+        console.log("hello world");
+
+    });
+
 
 }
 
-
-renderPups();
+async function render() {
+    await getPups();
+    console.log("getPups completed");
+    // CanvasRenderingContext2D();
+    renderPups();
+}
+render();
